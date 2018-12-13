684c734890338480dd55ba0984d4a25750cdeb03
francistse23 <wtse.ucsd@gmail.com>
2018-12-13 12:48:50 -0800

    Updates

---
+++ precache-manifest.08baf03a037ed7b6594f18483ac1710f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7d533ab09956ceb10fb",
+    "url": "/Conways-Life/static/js/runtime~main.a7d533ab.js"
+  },
+  {
+    "revision": "b83ad38bcc973f5e4ee4",
+    "url": "/Conways-Life/static/js/main.b83ad38b.chunk.js"
+  },
+  {
+    "revision": "5f679a792f4ad662044b",
+    "url": "/Conways-Life/static/js/1.5f679a79.chunk.js"
+  },
+  {
+    "revision": "b83ad38bcc973f5e4ee4",
+    "url": "/Conways-Life/static/css/main.ce87c9d3.chunk.css"
+  },
+  {
+    "revision": "5f679a792f4ad662044b",
+    "url": "/Conways-Life/static/css/1.80a78f08.chunk.css"
+  },
+  {
+    "revision": "67883ed26d511b29952697487e951605",
+    "url": "/Conways-Life/index.html"
+  }
+];
